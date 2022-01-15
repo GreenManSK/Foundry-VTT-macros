3f361370f43a33a1d842a1ae0a2b2bfc94f2815f
Lukáš Kurčík <lukas.kurcik@gmail.com>
2022-01-15 10:37:33 +0100

    Random gender for multiple tokens

--- random_gender.js
+++ random_gender.js
@@ -1,13 +1,14 @@
 (async () => {
-    const tok = canvas.tokens.controlled[0];
-    let raceDir = tok.data.img.split('/');
-    raceDir.splice(raceDir.length - 2, 2);
-    const raceContent = await FilePicker.browse("user", raceDir.join('/'));
-    const genderDir = raceContent.dirs[Math.floor(Math.random()*raceContent.dirs.length)];
+    const updates = [];
+    await Promise.all(canvas.tokens.controlled.map(async (token) => {
+        let raceDir = token.data.img.split('/');
+        raceDir.splice(raceDir.length - 2, 2);
+        const raceContent = await FilePicker.browse("user", raceDir.join('/'));
+        const genderDir = raceContent.dirs[Math.floor(Math.random() * raceContent.dirs.length)];
 
-    const content = await FilePicker.browse("user", genderDir);
-    const min = Math.ceil(0); const max = Math.floor(content.files.length);
-    const newImage = content.files[Math.floor(Math.random() * (max - min) + min)];
-    const updates = [{ _id: tok.id, img: newImage }];
+        const content = await FilePicker.browse("user", genderDir);
+        const newImage = content.files[Math.floor(Math.random() * content.files.length)];
+        updates.push({ _id: token.id, img: newImage });
+    }));
     canvas.scene.updateEmbeddedDocuments('Token', updates);
-})()
+})();
