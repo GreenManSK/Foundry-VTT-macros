b1a55c3ff6fe4b3c686c2c257885fbf574c75f41
Lukáš Kurčík <lukas.kurcik@gmail.com>
2022-01-15 10:35:29 +0100

    Make random face work on multiple tokens

--- random_face.js
+++ random_face.js
@@ -1,10 +1,13 @@
 (async () => {
-    const tok = canvas.tokens.controlled[0];
-    let dir = tok.data.img.split('/'); dir.splice(dir.length - 1, 1);
-    dir = dir.join('/');
-    const content = await FilePicker.browse("user", dir);
-    const min = Math.ceil(0); const max = Math.floor(content.files.length);
-    const newImage = content.files[Math.floor(Math.random() * (max - min) + min)];
-    const updates = [{ _id: tok.id, img: newImage }];
+    const updates = [];
+    await Promise.all(canvas.tokens.controlled.map(async (token) => {
+        let dir = token.data.img.split('/');
+        dir.splice(dir.length - 1, 1);
+        dir = dir.join('/');
+
+        const content = await FilePicker.browse("user", dir);
+        const newImage = content.files[Math.floor(Math.random() * content.files.length)];
+        updates.push({ _id: token.id, img: newImage });
+    }));
     canvas.scene.updateEmbeddedDocuments('Token', updates);
-})()
+})();
